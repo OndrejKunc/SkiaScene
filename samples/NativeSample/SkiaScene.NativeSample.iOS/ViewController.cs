b1901425890b40d3ac18def2898dd5559fe160a6
Ondřej Kunc <kunc.ondrej1@gmail.com>
2017-12-29 13:35:32 +0100

    Add iOS native sample

--- samples/NativeSample/SkiaScene.NativeSample.iOS/ViewController.cs
+++ samples/NativeSample/SkiaScene.NativeSample.iOS/ViewController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SkiaScene.iOS;
+using SkiaSharp;
+using SkiaSharp.Views.iOS;
+using System;
 
 using UIKit;
 
@@ -6,7 +9,11 @@ namespace SkiaScene.NativeSample.iOS
 {
     public partial class ViewController : UIViewController
     {
-        int count = 1;
+        private ISKScene _scene;
+        private ITouchManipulationManager _touchManipulationManager;
+        private ITouchManipulationRenderer _touchManipulationRenderer;
+        private SKCanvasView _canvasView;
+        private TouchHandler _touchHandler;
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -16,19 +23,52 @@ namespace SkiaScene.NativeSample.iOS
         {
             base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view, typically from a nib.
-            Button.AccessibilityIdentifier = "myButton";
-            Button.TouchUpInside += delegate
+            //TODO test this
+            _canvasView = new SKCanvasView();
+            View.Add(_canvasView);
+
+            _canvasView.PaintSurface += OnPaint;
+            _touchHandler = new TouchHandler();
+            _touchHandler.RegisterEvents(_canvasView);
+            _touchHandler.TouchAction += OnTouch;
+        }
+
+
+        private void OnTouch(object sender, TouchTracking.TouchActionEventArgs args)
+        {
+            SKPoint viewPoint = args.Location;
+            SKPoint point =
+                new SKPoint((float)(_canvasView.CanvasSize.Width * viewPoint.X / _canvasView.Frame.Width),
+                            (float)(_canvasView.CanvasSize.Height * viewPoint.Y / _canvasView.Frame.Height));
+
+            var actionType = args.Type;
+            _touchManipulationRenderer.Render(point, actionType, args.Id);
+        }
+
+        private void InitSceneObjects()
+        {
+            _scene = new SKScene(new TestScenereRenderer(), _canvasView.CanvasSize);
+            _touchManipulationManager = new TouchManipulationManager(_scene)
             {
-                var title = string.Format("{0} clicks!", count++);
-                Button.SetTitle(title, UIControlState.Normal);
+                TouchManipulationMode = TouchManipulationMode.ScaleRotate,
+            };
+            _touchManipulationRenderer = new TouchManipulationRenderer(_touchManipulationManager, () => _canvasView.SetNeedsDisplay())
+            {
+                MaxFramesPerSecond = 100,
             };
         }
 
-        public override void DidReceiveMemoryWarning()
+        private void OnPaint(object sender, SKPaintSurfaceEventArgs args)
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.		
+            if (_scene == null)
+            {
+                InitSceneObjects();
+
+            }
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+            _scene.Render(canvas);
         }
     }
 }
